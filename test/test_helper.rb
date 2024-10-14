ae77ac5402faeebd5d39363cc60dd72974858569
Florian Frank <flori@ping.de>
2024-10-14 17:37:15 +0200

    Refactor database setup in test helper
    - Removed `$ch` variable from connection establishment.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,6 @@ database = File.basename(database_url.path)
 database_url_without_db = database_url.dup.tap { _1.path = '' }
 ch = ActiveRecord::Base.establish_connection(database_url_without_db.to_s)
 ch.with_connection { _1.execute %{ CREATE DATABASE IF NOT EXISTS #{database} } }
-$ch = ActiveRecord::Base.establish_connection(database_url.to_s)
+ActiveRecord::Base.establish_connection(database_url.to_s)
 
 require 'test/unit'
